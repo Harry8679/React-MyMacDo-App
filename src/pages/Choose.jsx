9733019096ee2e0ad6ccb121af0aafd252a95f61
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-14 00:32:00 +0200

    Choose screen 2

---
+++ src/pages/Choose.jsx
@@ -0,0 +1,9 @@
+import { Box, Fade, Typography } from '@mui/material';
+import Logo from "../components/Logo";
+import { useStyles } from '../styles';
+
+const Choose = () => {
+    
+}
+
+export default Choose;
