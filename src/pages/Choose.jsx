62ca9e1864bd6c82a0dc890b13975d6c93f287b9
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-14 00:45:51 +0200

    Styles updated

--- src/pages/Choose.jsx
+++ src/pages/Choose.jsx
@@ -3,7 +3,10 @@ import Logo from "../components/Logo";
 import { useStyles } from '../styles';
 
 const Choose = () => {
-    
+    const styles = useStyles();
+    <Fade in={true}>
+        <Box className={[styles.root, styles.na]}
+    </Fade>
 }
 
 export default Choose;
