1d315f22b0c7e64ed28686b7604f6ce78fc48a6d
Harry EMARH <emarh.harry.code@gmail.com>
2025-09-29 22:13:30 +0200

    Choose 7

--- src/pages/Choose.jsx
+++ src/pages/Choose.jsx
@@ -1,42 +1,71 @@
-import { Box, Card, Fade, Typography } from '@mui/material';
+import { Box, Card, Fade, Typography } from "@mui/material";
 import Logo from "../components/Logo";
-import { useStyles } from '../styles';
-import { CardActionArea, CardContent, CardMedia } from '@material-ui/core';
+import { useStyles } from "../styles";
+import { CardActionArea, CardContent, CardMedia } from "@mui/material";
 import clsx from "clsx";
 
 const Choose = () => {
-    const styles = useStyles();
+  const styles = useStyles();
+  return (
     <Fade in={true}>
-        <Box className={[styles.root, styles.navy]}>
-            <Box className={[styles.main, styles.center]}>
-                <Logo large></Logo>
-                <Typography variant='h3' component='h3' className={styles.center} gutterBottom>Where will you be eating today ?</Typography>
-                <Box className={styles.cards}>
-                    <Card className={[styles.card, styles.space]}>
-                        <CardActionArea>
-                            <CardMedia component="img" alt="Eat in" image="/images/eatin.png" className={styles.media} />
-                            <CardContent>
-                                <Typography gutterBottom variant="h4" color="textPrimary" component="p">
-                                    Eat In
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
+      <Box className={clsx(styles.root, styles.navy)}>
+        <Box className={clsx(styles.main, styles.center)}>
+          <Logo large />
+          <Typography
+            variant="h3"
+            component="h3"
+            className={styles.center}
+            gutterBottom
+          >
+            Where will you be eating today ?
+          </Typography>
+          <Box className={styles.cards}>
+            <Card className={clsx(styles.card, styles.space)}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Eat in"
+                  image="/images/eatin.png"
+                  className={styles.media}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h4"
+                    color="textPrimary"
+                    component="p"
+                  >
+                    Eat In
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
 
-                    <Card className={clsx(styles.card, styles.space)}>
-                        <CardActionArea>
-                            <CardMedia component="img" alt="Eat in" image="/images/takeout.png" className={styles.media} />
-                            <CardContent>
-                                <Typography gutterBottom variant="h4" color="textPrimary" component="p">
-                                    Take out
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Box>
-            </Box>
+            <Card className={clsx(styles.card, styles.space)}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Take out"
+                  image="/images/takeout.png"
+                  className={styles.media}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h4"
+                    color="textPrimary"
+                    component="p"
+                  >
+                    Take out
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Box>
         </Box>
+      </Box>
     </Fade>
-}
+  );
+};
 
 export default Choose;
